ae980f54ea8f0fbd5a21679544340413f57ec8ac
Ammar Faizi <ammarfaizi2@gmail.com>
2019-04-26 13:13:16 +0700

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>*{font-family: Tahoma;}</style>
+</head>
+<body>
+<center>
+	<h1>This site is down!</h1>
+	<h3><?php print $_SERVER["HTTP_HOST"]; ?></h3>
+</center>
+</body>
+</html>
+
